36c8fda4ce504c2844384fd7ceff115760df5498
Jonatan Souza <jonatan.souza04@gmail.com>
2020-09-03 20:18:44 -0300

    Adicionado a API de localização

--- js/main.js
+++ js/main.js
@@ -6,5 +6,26 @@ function helloWorld() {
     console.log('Hello World!');
    }
    
-   helloWorld();
+helloWorld();
+
+var componentMaps = document.getElementById("location");
+
+function getLocation() {
+   if (navigator.geolocation) 
+   navigator.geolocation.getCurrentPosition(viewLocation) 
+}
+
+function viewLocation(position) {
+  if(position.coords.latitude !== undefined)
+  {
+   componentMaps.src = 'https://maps.google.com/maps?q=' + position.coords.latitude + ',' + position.coords.longitude + '&t=&z=13&ie=UTF8&iwloc=&output=embed';
+   var componentDisplay = document.getElementById("location-display");
+   componentDisplay.style.display = 'block';
+
+   document.getElementById('detail-location').innerHTML = 'Latitude: ' + position.coords.latitude + '  -  Longitude: ' + position.coords.longitude; 
+  }
+}
+
+//Faz chamada da localização
+getLocation();
    
